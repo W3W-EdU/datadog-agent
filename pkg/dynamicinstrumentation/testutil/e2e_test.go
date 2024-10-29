3db5da2353b04bb0019d359cf1435419528487f4
grantseltzer <grantseltzer@gmail.com>
2024-10-28 21:17:13 -0500

    Simplify the way locations are assigned, remove corrections related to previous way of generating code, also make locations pointers
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- pkg/dynamicinstrumentation/testutil/e2e_test.go
+++ pkg/dynamicinstrumentation/testutil/e2e_test.go
@@ -195,7 +195,7 @@ var configTemplateText = `
             ],
             "captureSnapshot": false,
             "capture": {
-                "maxReferenceDepth": 6
+                "maxReferenceDepth": 10
             },
             "sampling": {
                 "snapshotsPerSecond": 5000
